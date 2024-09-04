75c7fde8c128239594d065d14102e5ed482c63e7
Revcy <Revcy.unity@gmail.com>
2024-09-04 23:54:28 +0700

    Basic Cards Made
    All basic card functions made
    (Numbers bug might be present)

--- Assets/_Scripts/Card/CardVariants/Basic/MagicBuffer/MagicBuffer.cs
+++ Assets/_Scripts/Card/CardVariants/Basic/MagicBuffer/MagicBuffer.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "MagicBuffer", menuName = "ScriptableObjects/Cards/MagicBuffer")]
 
 public class MagicBuffer : Card
 {
-    public override bool Play(Entity from, Entity[] target)
+        public override bool Play(Entity from, Entity[] target)
     {
-        return true;
+        if (target.Length > 0)
+        {
+            var targetList = target.ToList();
+            var ally = targetList.Select((target) => target.GetComponent<Character>());
+
+            if (!ally.Any()) return false;
+
+            foreach (var character in ally)
+            {
+                character.ApplyStatusEffect(from, statusEffectToApply[0]);
+                CardManager.instance.DrawCard();
+            }
+
+            return true;
+        }
+        else return false;
     }
 }
